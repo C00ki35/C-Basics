8f2c19d5ae6d66cc6e0481b39d77a693078f9733
Paul <pgcdigit@gmail.com>
2020-06-29 09:10:41 +0100

    basic var types added

--- vars/vars/Program.cs
+++ vars/vars/Program.cs
@@ -10,7 +10,19 @@ namespace vars
     {
         static void Main(string[] args)
         {
-            //testing
+            var number = 1;
+            var count = 33;
+            var total = 3.3;
+            var firstName = "Peter";
+            var letter = 'B';
+            var isWorking = false;
+
+            Console.WriteLine(count);
+            Console.WriteLine(number);
+            Console.WriteLine(firstName);
+            Console.WriteLine(letter);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(total);
         }
     }
 }
