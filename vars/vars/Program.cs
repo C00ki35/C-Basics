2d0b38eb4e7e9c89ba17cba58ece0ec281a66447
Paul <pgcdigit@gmail.com>
2020-06-29 10:56:58 +0100

    Basic operators

--- vars/vars/Program.cs
+++ vars/vars/Program.cs
@@ -11,6 +11,18 @@ namespace vars
         static void Main(string[] args)
         {
             //operators
+
+            var a = 10;
+            var b = 20;
+            var c = 4;
+
+            Console.WriteLine((float)a/(float)b);
+            Console.WriteLine((a * b) + c);
+            Console.WriteLine(a > b);
+            Console.WriteLine(a != c);
+            Console.WriteLine(c < b && c < a);
+
+
         }
     }
 }
