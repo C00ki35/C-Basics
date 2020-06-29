e5f509645b215e8fddf3688e2dd0bfddd0f8db63
Paul <pgcdigit@gmail.com>
2020-06-29 09:33:10 +0100

    Arithmetic operators

--- vars/vars/Program.cs
+++ vars/vars/Program.cs
@@ -10,39 +10,7 @@ namespace vars
     {
         static void Main(string[] args)
         {
-            var number = 1;
-            var count = 33;
-            var total = 3.3;
-            var firstName = "Peter";
-            var letter = 'B';
-            var isWorking = false;
-
-            Console.WriteLine(count);
-            Console.WriteLine(number);
-            Console.WriteLine(firstName);
-            Console.WriteLine(letter);
-            Console.WriteLine(isWorking);
-            Console.WriteLine(total);
-
-
-            //Conversion types
-
-            string numToString = "1234";
-            int i = Convert.ToInt32(numToString);
-
-            //Handling exceptions
-
-            try
-            {
-                var n = "1223";
-                byte b = Convert.ToByte(n);
-                Console.WriteLine(b);
-            }
-            catch (Exception)
-            {
-
-                Console.WriteLine("The number could not be converted to a byte");
-            }
+            //operators
         }
     }
 }
