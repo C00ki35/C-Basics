03ff75f8b102fc7faf906039e712225b24064634
Paul <pgcdigit@gmail.com>
2020-07-01 08:52:45 +0100

    Basic enum setup

--- vars/vars/Program.cs
+++ vars/vars/Program.cs
@@ -11,29 +11,8 @@ namespace vars
     {
         static void Main(string[] args)
         {
-            //BASIC ARRAYS
-            var numbers = new int[3];
-            numbers[0] = 1;
+           //Enums
 
-            var flags = new bool[3];
-            flags[0] = true;
-
-            var names = new string[3] { "Jack", "Jill", "Tim" };
-
-
-            //STRINGS
-            string myname = string.Format("{0} {1}", "Dave", "Simpson");
-
-            string firstName = "Ken";
-            Console.WriteLine(firstName[0]);
-
-            var sentence = "My name is \"Ken\"";
-            Console.WriteLine(sentence);
-
-            //Verbatim string - doesn't work with double quotes.
-            Console.WriteLine(@"This \ is / // String");
-            var didMatch = string.Compare("Ken", "Ken");
-            Console.WriteLine(didMatch);
         }
     }
 }
