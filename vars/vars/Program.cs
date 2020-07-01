e1d41ba192a46142a953915e59a640d463c3d64f
Paul <pgcdigit@gmail.com>
2020-07-01 08:45:44 +0100

    Basic arrays and strings

--- vars/vars/Program.cs
+++ vars/vars/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using vars.Math;
+
 
 namespace vars
 {
@@ -11,16 +11,29 @@ namespace vars
     {
         static void Main(string[] args)
         {
-            var Ken = new Person();
-            Ken.FirstName = "Ken";
-            Ken.LastName = "Simms";
-            Ken.Introduce();
+            //BASIC ARRAYS
+            var numbers = new int[3];
+            numbers[0] = 1;
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 8);
-            Console.WriteLine(result);
-           
+            var flags = new bool[3];
+            flags[0] = true;
 
+            var names = new string[3] { "Jack", "Jill", "Tim" };
+
+
+            //STRINGS
+            string myname = string.Format("{0} {1}", "Dave", "Simpson");
+
+            string firstName = "Ken";
+            Console.WriteLine(firstName[0]);
+
+            var sentence = "My name is \"Ken\"";
+            Console.WriteLine(sentence);
+
+            //Verbatim string - doesn't work with double quotes.
+            Console.WriteLine(@"This \ is / // String");
+            var didMatch = string.Compare("Ken", "Ken");
+            Console.WriteLine(didMatch);
         }
     }
 }
