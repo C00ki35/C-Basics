85539972c5a88b452360e1a58a8bdeeec27b87a8
Paul <pgcdigit@gmail.com>
2020-07-01 09:23:41 +0100

    Accessing enum values - casting string to enum

--- vars/vars/Program.cs
+++ vars/vars/Program.cs
@@ -7,12 +7,32 @@ using System.Threading.Tasks;
 
 namespace vars
 {
+
+    public enum ShippingMethod
+    {
+        RegularAirmail = 1,
+        RegisteredAirmail = 2,
+        Express = 3
+    }
     class Program
     {
         static void Main(string[] args)
         {
-           //Enums
+            //Enums
 
-        }
+
+            //Casting to value
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            //Casting to string value
+            var Smethod = 2;
+            Console.WriteLine((ShippingMethod)Smethod);
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+            }
     }
+
+
 }
